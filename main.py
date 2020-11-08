2656d3f3ad4a9c10c325356cce31c3907832a676
naberatu <naberatu@gmail.com>
2020-11-07 18:35:47 -0800

    Established accuracy readings.

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ import torchvision.transforms as transforms
 import numpy as np
 import matplotlib.pyplot as plt
 
+# Path to save/load from
+PATH = './cifar_net.pth'
+
 # Loading CIFAR-10
 transform = transforms.Compose(
             [transforms.ToTensor(),
@@ -46,7 +49,7 @@ if __name__ == '__main__':
     # ================================================================
     dataiter = iter(testloader)
     images, labels = dataiter.next()
-    imshow(torchvision.utils.make_grid(images))
+    # imshow(torchvision.utils.make_grid(images))
     print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
     # Base Network
@@ -70,23 +73,55 @@ if __name__ == '__main__':
             x = self.fc3(x)
             return x
 
-
     net = Net()
-    PATH = './cifar_net.pth'
     net.load_state_dict(torch.load(PATH))
 
     outputs = net(images)
 
     _, predicted = torch.max(outputs, 1)
-
     print('Predicted: ', ' '.join('%5s' % classes[predicted[j]]
                                   for j in range(4)))
+
+    # Test Run: Overall
+    # ==========================================================
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('\nAccuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+
+    # Test Run: Breakdown
+    # ==========================================================
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
 
     # criterion = nn.CrossEntropyLoss()
     # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
 
-    # # Training algorithm
-    # # ======================================================================
+    # Training algorithm
+    # ======================================================================
+
     # for epoch in range(2):  # loop over the dataset multiple times
     #     running_loss = 0.0
     #     for i, data in enumerate(trainloader, 0):
