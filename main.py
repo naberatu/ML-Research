39854ea9039fb85246001676873d8cf08518cfd5
naberatu <naberatu@gmail.com>
2020-11-07 17:58:43 -0800

    initial commit part 2

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ transform = transforms.Compose(
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
 
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
-                                                download=False, transform=transform)
+                                                download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
                                                  shuffle=False, num_workers=2)
 
