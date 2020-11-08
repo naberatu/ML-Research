3fb3dbe46ba653708c5b9e0c7c1010440da01c97
naberatu <naberatu@gmail.com>
2020-11-07 18:01:40 -0800

    CIFAR set downloaded.

--- main.py
+++ main.py
@@ -13,10 +13,10 @@ transform = transforms.Compose(
 trainset = torchvision.datasets.CIFAR10(root='./data', train=True,
                                                 download=True, transform=transform)
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=4,
-                                                 shuffle=False, num_workers=2)
+                                                 shuffle=True, num_workers=2)
 
 testset = torchvision.datasets.CIFAR10(root='./data', train=False,
-                                               download=False, transform=transform)
+                                               download=True, transform=transform)
 testloader = torch.utils.data.DataLoader(testset, batch_size=4,
                                                  shuffle=False, num_workers=2)
 
