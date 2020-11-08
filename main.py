d2bb47fdec67a3cda8175773d4eb14ba3bc69247
naberatu <naberatu@gmail.com>
2020-11-07 19:52:47 -0800

    Improved epoch training method.

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 
 # Path to save/load from
 PATH = './cifar_net.pth'
+DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 # Loading CIFAR-10
 transform = transforms.Compose(
@@ -38,28 +39,22 @@ def imshow(img):
 
 if __name__ == '__main__':
 
-    # # Training Iterator
-    # # ================================================================
-    # dataiter = iter(trainloader)
-    # images, labels = dataiter.next()
-    # imshow(torchvision.utils.make_grid(images))
-    # print(' '.join('%5s' % classes[labels[j]] for j in range(4)))
-
-    # Testing Iterator
+    # Data Iterator
     # ================================================================
+    # dataiter = iter(trainloader)
     dataiter = iter(testloader)
     images, labels = dataiter.next()
     # imshow(torchvision.utils.make_grid(images))
-    print('GroundTruth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    print('Truth: ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
 
     # Base Network
     # =====================================================================
     class Net(nn.Module):
         def __init__(self):
             super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 6, 5)
+            self.conv1 = nn.Conv2d(3, 8, 5)
             self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(6, 16, 5)
+            self.conv2 = nn.Conv2d(8, 16, 5)
             self.fc1 = nn.Linear(16 * 5 * 5, 120)
             self.fc2 = nn.Linear(120, 84)
             self.fc3 = nn.Linear(84, 10)
@@ -74,8 +69,39 @@ if __name__ == '__main__':
             return x
 
     net = Net()
-    net.load_state_dict(torch.load(PATH))
 
+    # net.to(DEVICE)  # Only for training
+    # criterion = nn.CrossEntropyLoss()
+    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
+
+    # Training algorithm
+    # ======================================================================
+    # for epoch in range(10):  # loop over the dataset multiple times
+    #     running_loss = 0.0
+    #     for i, data in enumerate(trainloader, 0):
+    #         # get the inputs; data is a list of [inputs, labels]
+    #         inputs, labels = data[0].to(DEVICE), data[1].to(DEVICE)
+    #
+    #         # zero the parameter gradients
+    #         optimizer.zero_grad()
+    #
+    #         # forward + backward + optimize
+    #         outputs = net(inputs)
+    #         loss = criterion(outputs, labels)
+    #         loss.backward()
+    #         optimizer.step()
+    #
+    #         # print statistics
+    #         running_loss += loss.item()
+    #         if i % 2000 == 1999:    # print every 2000 mini-batches
+    #             print('[%d, %5d] loss: %.3f' %
+    #                   (epoch + 1, i + 1, running_loss / 2000))
+    #             running_loss = 0.0
+    #
+    # print('Finished Training')
+    # torch.save(net.state_dict(), PATH)
+
+    net.load_state_dict(torch.load(PATH))
     outputs = net(images)
 
     _, predicted = torch.max(outputs, 1)
@@ -115,36 +141,3 @@ if __name__ == '__main__':
     for i in range(10):
         print('Accuracy of %5s : %2d %%' % (
             classes[i], 100 * class_correct[i] / class_total[i]))
-
-    # criterion = nn.CrossEntropyLoss()
-    # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
-
-    # Training algorithm
-    # ======================================================================
-
-    # for epoch in range(2):  # loop over the dataset multiple times
-    #     running_loss = 0.0
-    #     for i, data in enumerate(trainloader, 0):
-    #         # get the inputs; data is a list of [inputs, labels]
-    #         inputs, labels = data
-    #
-    #         # zero the parameter gradients
-    #         optimizer.zero_grad()
-    #
-    #         # forward + backward + optimize
-    #         outputs = net(inputs)
-    #         loss = criterion(outputs, labels)
-    #         loss.backward()
-    #         optimizer.step()
-    #
-    #         # print statistics
-    #         running_loss += loss.item()
-    #         if i % 2000 == 1999:    # print every 2000 mini-batches
-    #             print('[%d, %5d] loss: %.3f' %
-    #                   (epoch + 1, i + 1, running_loss / 2000))
-    #             running_loss = 0.0
-    #
-    # print('Finished Training')
-    #
-    # PATH = './cifar_net.pth'
-    # torch.save(net.state_dict(), PATH)
