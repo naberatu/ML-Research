2a1d9a243f9607bbbe98b2b15c622a565eae567b
naberatu <naberatu@gmail.com>
2020-11-08 18:00:32 -0800

    70% Success rate + reorganized files.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from net import Net
 import torchvision
 import torchvision.transforms as transforms
+import torch.optim as optim
 import numpy as np
 import matplotlib.pyplot as plt
 from PIL import Image
 
-# Path to save/load from
+# Parameters
+# ============================================================================================
 PATH = './cifar_net.pth'
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-IMSIZE = 256
-LOADER = transforms.Compose([transforms.Scale(IMSIZE), transforms.ToTensor()])
+# IMSIZE = 256
+# LOADER = transforms.Compose([transforms.Scale(IMSIZE), transforms.ToTensor()])
 
 # Loading CIFAR-10
+# ============================================================================================
 transform = transforms.Compose(
             [transforms.ToTensor(),
              transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
@@ -31,6 +35,8 @@ testloader = torch.utils.data.DataLoader(testset, batch_size=4,
 classes = ('plane', 'car', 'bird', 'cat',
            'deer', 'dog', 'frog', 'horse', 'ship', 'truck')
 
+# stlset = torchvision.datasets.STL10(root='./data', download=True)
+
 
 def imshow(img):
     img = img / 2 + 0.5     # unnormalize
@@ -39,55 +45,34 @@ def imshow(img):
     plt.show()
 
 
-def imload(img):
-    image = Image.open(img)
-    image = LOADER(image).float()
-    # image = (image, requires_grad=True)
-    image = image.unsqueeze(0)  # this is for VGG, may not be needed for ResNet
-    return image.cuda()  # assumes that you're using GPU
+# def imload(img):
+#     image = Image.open(img)
+#     image = LOADER(image).float()
+#     # image = (image, requires_grad=True)
+#     image = image.unsqueeze(0)  # this is for VGG, may not be needed for ResNet
+#     return image.cuda()  # assumes that you're using GPU
 
-IMAGE = imload("C:\\Users\\elite\\Desktop\\cat.png")
+# IMAGE = imload('C:\\Users\\elite\\Desktop\\cat.png')
 
 if __name__ == '__main__':
 
+    net = Net()    # Summon Neural Net
+
     # Data Iterator
     # ================================================================
-    # dataiter = iter(trainloader)
-    dataiter = iter(testloader)
+    dataiter = iter(trainloader)
+    # dataiter = iter(testloader)
     images, labels = dataiter.next()
-    print('Truth:\t\t ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
-    # imshow(torchvision.utils.make_grid(images))
+    print('| Truth:\t\t ', ' '.join('%5s' % classes[labels[j]] for j in range(4)))
+    # imshow(torchvision.utils.make_grid(images))   # Displays images
 
-    # Base Network
-    # =====================================================================
-    class Net(nn.Module):
-        def __init__(self):
-            super(Net, self).__init__()
-            self.conv1 = nn.Conv2d(3, 32, 5)
-            self.pool = nn.MaxPool2d(2, 2)
-            self.conv2 = nn.Conv2d(32, 16, 5)
-            self.fc1 = nn.Linear(16 * 5 * 5, 120)
-            self.fc2 = nn.Linear(120, 84)
-            self.fc3 = nn.Linear(84, 10)
-
-        def forward(self, x):
-            x = self.pool(F.relu(self.conv1(x)))
-            x = self.pool(F.relu(self.conv2(x)))
-            x = x.view(-1, 16 * 5 * 5)
-            x = F.relu(self.fc1(x))
-            x = F.relu(self.fc2(x))
-            x = self.fc3(x)
-            return x
-
-    net = Net()
-
+    # # Training algorithm
+    # # ======================================================================
     # net.to(DEVICE)  # Only for training
     # criterion = nn.CrossEntropyLoss()
     # optimizer = optim.SGD(net.parameters(), lr=0.001, momentum=0.9)
     #
-    # # Training algorithm
-    # # ======================================================================
-    # for epoch in range(16):  # loop over the dataset multiple times
+    # for epoch in range(12):  # loop over the dataset multiple times
     #     running_loss = 0.0
     #     for i, data in enumerate(trainloader, 0):
     #         # get the inputs; data is a list of [inputs, labels]
@@ -112,44 +97,43 @@ if __name__ == '__main__':
     # print('Finished Training')
     # torch.save(net.state_dict(), PATH)
 
+    # Neural Net` Testing
+    # ==========================================================
     net.load_state_dict(torch.load(PATH))
-    net(IMAGE)
-    # outputs = net(images)
+    outputs = net(images)
+    # net(IMAGE)
+    _, predicted = torch.max(outputs, 1)
+    print('| Predicted:\t ', ' '.join('%5s' % classes[predicted[j]]
+                                    for j in range(4)))
 
-    # _, predicted = torch.max(outputs, 1)
-    # print('Predicted:\t ', ' '.join('%5s' % classes[predicted[j]]
-    #                               for j in range(4)))
-    #
-    # # Test Run: Overall
-    # # ==========================================================
-    # correct = 0
-    # total = 0
-    # with torch.no_grad():
-    #     for data in testloader:
-    #         images, labels = data
-    #         outputs = net(images)
-    #         _, predicted = torch.max(outputs.data, 1)
-    #         total += labels.size(0)
-    #         correct += (predicted == labels).sum().item()
-    #
-    # print('\nAccuracy of the network on the 10000 test images: %d %%' % (
-    #         100 * correct / total))
-    #
-    # # Test Run: Breakdown
-    # # ==========================================================
-    # class_correct = list(0. for i in range(10))
-    # class_total = list(0. for i in range(10))
-    # with torch.no_grad():
-    #     for data in testloader:
-    #         images, labels = data
-    #         outputs = net(images)
-    #         _, predicted = torch.max(outputs, 1)
-    #         c = (predicted == labels).squeeze()
-    #         for i in range(4):
-    #             label = labels[i]
-    #             class_correct[label] += c[i].item()
-    #             class_total[label] += 1
-    #
-    # for i in range(10):
-    #     print('Accuracy of %5s : %2d %%' % (
-    #         classes[i], 100 * class_correct[i] / class_total[i]))
+    # Test Run: Overall
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    print('\n| Accuracy of the network on the 10000 test images: %d %%' % (
+            100 * correct / total))
+
+    # Test Run: Breakdown
+    class_correct = list(0. for i in range(10))
+    class_total = list(0. for i in range(10))
+    with torch.no_grad():
+        for data in testloader:
+            images, labels = data
+            outputs = net(images)
+            _, predicted = torch.max(outputs, 1)
+            c = (predicted == labels).squeeze()
+            for i in range(4):
+                label = labels[i]
+                class_correct[label] += c[i].item()
+                class_total[label] += 1
+
+    for i in range(10):
+        print('| Accuracy of %5s : %2d %%' % (
+            classes[i], 100 * class_correct[i] / class_total[i]))
---
+++ net.py
@@ -0,0 +1,27 @@
+
+import torch.nn as nn
+import torch.nn.functional as F
+IMF = 1     # For quickly modifying dimensional parameters
+
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.conv1 = nn.Conv2d(3, 32, 5)
+        self.pool = nn.MaxPool2d(2, 2)
+        self.conv2 = nn.Conv2d(32, 64, 5)
+        self.conv3 = nn.Conv2d(64, 32, 5)
+        self.fc1 = nn.Linear(32 * IMF * IMF, 120)
+        self.fc2 = nn.Linear(120, 84)
+        self.fc3 = nn.Linear(84, 10)
+
+    def forward(self, x):
+        x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        # print(x.shape)
+        x = x.view(-1, 32 * IMF * IMF)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
