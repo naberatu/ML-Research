8c3b38ab6ab19e944344a9a52c93b4a65051ec2c
naberatu <naberatu@gmail.com>
2021-01-28 13:48:44 -0800

    Datloader and iterator seem to work just fine.

--- covct.py
+++ covct.py
@@ -1,60 +1,141 @@
+
+import torch
+import torch.nn as nn
+from torch.utils.data import Dataset, DataLoader
+from torchvision import transforms as transforms
+import os
+import cv2
+import random
+import matplotlib.pyplot as plt
+import torch.optim as optim
+from PIL import Image
+from torch.utils.tensorboard import SummaryWriter
+import glob
+import shutil
+import numpy as np
+
+
+def read_txt(txt_path):
+    with open(txt_path) as f:
+        lines = f.readlines()
+    txt_data = [line.strip() for line in lines]
+    return txt_data
+
+
+class CovidCTDataset(Dataset):
+    def __init__(self, root_dir, classes, covid_files, non_covid_files, transform=None):
+        self.root_dir = root_dir
+        self.classes = classes
+        self.files_path = [non_covid_files, covid_files]
+        self.image_list = []
+
+        # read the files from data split text files
+        covid_files = read_txt(covid_files)
+        non_covid_files = read_txt(non_covid_files)
+
+        # combine the positive and negative files into a cummulative files list
+        for cls_index in range(len(self.classes)):
+            class_files = [[os.path.join(self.root_dir, self.classes[cls_index], x), cls_index] \
+                           for x in read_txt(self.files_path[cls_index])]
+            self.image_list += class_files
+
+        self.transform = transform
+
+    def __len__(self):
+        return len(self.image_list)
+
+    def __getitem__(self, idx):
+        path = self.image_list[idx][0]
+
+        # Read the image
+        image = Image.open(path).convert('RGB')
+
+        # Apply transforms
+        if self.transform:
+            image = self.transform(image)
+
+        label = int(self.image_list[idx][1])
+
+        data = {'img': image,
+                'label': label,
+                'paths': path}
+
+        return data
+
+
+# def compute_metrics(model, test_loader, device, plot_roc_curve=False):
+#     model.eval()
 #
-# from __future__ import print_function, division
-# from PIL import Image
-# import os
-# import os.path
-# import numpy as np
-# import pickle
-# from typing import Any, Callable, Optional, Tuple
-# import pandas as pd
-# import matplotlib as plt
-# import torch
-# import torchvision
-# from torch.utils.data import Dataset, DataLoader
-# from torchvision import transforms, utils
+#     val_loss = 0
+#     val_correct = 0
 #
-# # Ignore warnings
-# import warnings
-# warnings.filterwarnings("ignore")
+#     criterion = nn.CrossEntropyLoss()
 #
-# def sample_scans():
-#     covid_files_path = './data/covct/'
-#     covid_files = [os.path.join(covid_files_path, x) for x in os.listdir(covid_files_path)]
-#     covid_images = [cv2.imread(x) for x in random.sample(covid_files, 5)]
+#     score_list = torch.Tensor([]).to(device)
+#     pred_list = torch.Tensor([]).to(device).long()
+#     target_list = torch.Tensor([]).to(device).long()
+#     path_list = []
 #
-#     plt.figure(figsize=(20, 10))
-#     columns = 5
-#     for i, image in enumerate(covid_images):
-#         plt.subplot(len(covid_images) / columns + 1, columns, i + 1)
-#         plt.imshow(image)
+#     for iter_num, data in enumerate(test_loader):
+#         # Convert image data into single channel data
+#         image, target = data['img'].to(device), data['label'].to(device)
+#         paths = data['paths']
+#         path_list.extend(paths)
 #
+#         # Compute the loss
+#         with torch.no_grad():
+#             output = model(image)
 #
-# class CovCT(Dataset):
-#     def __init__(self, root_dir="./data/covct/", train=True, transform=None):
-#         self.root_dir = root_dir
-#         self.transform = transform
-#         self.train = True
-#         self.covid_list = []
-#         self.nocov_list = []
+#         # Log loss
+#         val_loss += criterion(output, target.long()).item()
 #
-#         start1, start2, finish1, finish2 = 0, 0, 0, 0
-#         if train:
-#             start1, start2, finish1, finish2 = 1, 1, 626, 615
-#         else:
-#             start1, start2, finish1, finish2 = 627, 616, 1252, 1230
+#         # Calculate the number of correctly classified examples
+#         pred = output.argmax(dim=1, keepdim=True)
+#         val_correct += pred.eq(target.long().view_as(pred)).sum().item()
 #
-#         for i in range(start1, finish1):
-#             self.covid_list.append("Covid (" + str(i) + ").png")
-#         for i in range(start2, finish2):
-#             self.nocov_list.append("Non-Covid (" + str(i) + ").png")
+#         # Bookkeeping
+#         score_list = torch.cat([score_list, nn.Softmax(dim=1)(output)[:, 1].squeeze()])
+#         pred_list = torch.cat([pred_list, pred.squeeze()])
+#         target_list = torch.cat([target_list, target.squeeze()])
 #
-#     def __getitem__(self, idx):
-#         if torch.is_tensor(idx):
-#             idx = idx.tolist()
+#     classification_metrics = classification_report(target_list.tolist(), pred_list.tolist(),
+#                                                    target_names=['CT_NonCOVID', 'CT_COVID'],
+#                                                    output_dict=True)
 #
-#         return self.root_dir + self.covid_list[idx]
+#     # sensitivity is the recall of the positive class
+#     sensitivity = classification_metrics['CT_COVID']['recall']
 #
+#     # specificity is the recall of the negative class
+#     specificity = classification_metrics['CT_NonCOVID']['recall']
 #
+#     # accuracy
+#     accuracy = classification_metrics['accuracy']
 #
+#     # confusion matrix
+#     conf_matrix = confusion_matrix(target_list.tolist(), pred_list.tolist())
 #
+#     # roc score
+#     roc_score = roc_auc_score(target_list.tolist(), score_list.tolist())
 #
+#     # plot the roc curve
+#     if plot_roc_curve:
+#         fpr, tpr, _ = roc_curve(target_list.tolist(), score_list.tolist())
+#         plt.plot(fpr, tpr, label="Area under ROC = {:.4f}".format(roc_score))
+#         plt.legend(loc='best')
+#         plt.xlabel('False Positive Rate')
+#         plt.ylabel('True Positive Rate')
+#         plt.show()
+#
+#     # put together values
+#     metrics_dict = {"Accuracy": accuracy,
+#                     "Sensitivity": sensitivity,
+#                     "Specificity": specificity,
+#                     "Roc_score": roc_score,
+#                     "Confusion Matrix": conf_matrix,
+#                     "Validation Loss": val_loss / len(test_loader),
+#                     "score_list": score_list.tolist(),
+#                     "pred_list": pred_list.tolist(),
+#                     "target_list": target_list.tolist(),
+#                     "paths": path_list}
+#
+#     return metrics_dict
--- main.py
+++ main.py
@@ -12,6 +12,7 @@ from torch.utils.tensorboard import SummaryWriter
 import glob
 import shutil
 import numpy as np
+from covct import CovidCTDataset
 
 random.seed(0)
 
@@ -21,18 +22,77 @@ PATH = './data/covct/'
 DEVICE = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
 
-if __name__ == '__main__':
-    covid_files_path = PATH + "CT_COVID"
-    nocov_files_path = PATH + "CT_NonCOVID"
-    covid_files = [os.path.join(covid_files_path, x) for x in os.listdir(covid_files_path)]
-    covid_images = [cv2.imread(x) for x in random.sample(covid_files, 5)]
+def imshow(img):
+    # img = img / 5 + 1     # unnormalize
 
     plt.figure(figsize=(20, 10))
     columns = 5
-    for i, image in enumerate(covid_images):
-        plt.subplot(int(len(covid_images) / columns) + 1, columns, i + 1)
-        plt.imshow(image)
+    for i, image in enumerate(img):
+        plt.subplot(int(len(img) / columns) + 1, columns, i + 1)
+        plt.imshow(image[0])
+    # for i in img:
+    #     npimg = i.numpy()
+    #     plt.imshow(np.transpose(npimg, (1, 2, 0)))
     plt.show()
 
+
+if __name__ == '__main__':
+    covid_files_path = PATH + "CT_COVID"
+    nocov_files_path = PATH + "CT_NonCOVID"
+    # covid_files = [os.path.join(covid_files_path, x) for x in os.listdir(covid_files_path)]
+    # covid_images = [cv2.imread(x) for x in random.sample(covid_files, 5)]
+
+    # plt.figure(figsize=(20, 10))
+    # columns = 5
+    # for i, image in enumerate(covid_images):
+    #     plt.subplot(int(len(covid_images) / columns) + 1, columns, i + 1)
+    #     plt.imshow(image)
+    # plt.show()
+
+    normalize = transforms.Normalize(mean=[0, 0, 0], std=[1, 1, 1])
+    train_transformer = transforms.Compose([
+        transforms.Resize(256),
+        transforms.RandomResizedCrop((224), scale=(0.5, 1.0)),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        normalize
+    ])
+    val_transformer = transforms.Compose([
+        transforms.Resize((224, 224)),
+        transforms.ToTensor(),
+        normalize
+    ])
+
+    batchsize = 8
+    trainset = CovidCTDataset(root_dir=PATH,
+                              classes=['CT_NonCOVID', 'CT_COVID'],
+                              covid_files=PATH + 'Data-split/COVID/trainCT_COVID.txt',
+                              non_covid_files=PATH + 'Data-split/NonCOVID/trainCT_NonCOVID.txt',
+                              transform=train_transformer)
+    valset = CovidCTDataset(root_dir=PATH,
+                            classes=['CT_NonCOVID', 'CT_COVID'],
+                            covid_files=PATH + 'Data-split/COVID/valCT_COVID.txt',
+                            non_covid_files=PATH + 'Data-split/NonCOVID/valCT_NonCOVID.txt',
+                            transform=val_transformer)
+    testset = CovidCTDataset(root_dir=PATH,
+                             classes=['CT_NonCOVID', 'CT_COVID'],
+                             covid_files=PATH + 'Data-split/COVID/testCT_COVID.txt',
+                             non_covid_files=PATH + 'Data-split/NonCOVID/testCT_NonCOVID.txt',
+                             transform=val_transformer)
+
+    train_loader = DataLoader(trainset, batch_size=batchsize, drop_last=False, shuffle=True)
+    val_loader = DataLoader(valset, batch_size=batchsize, drop_last=False, shuffle=False)
+    test_loader = DataLoader(testset, batch_size=batchsize, drop_last=False, shuffle=False)
+
+    diter_train = iter(train_loader)
+    diter_val = iter(val_loader)
+    diter_test = iter(test_loader)
+
+    data = diter_train.__next__()
+    image = data["img"]
+    label = data["label"]
+    print(image)
+    imshow(image)
+    print(label)
 
 
