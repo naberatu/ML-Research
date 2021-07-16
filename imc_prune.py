8fefb2ef034d5bee0626cbbc107ea694a00c5db5
naberatu <naberatu@gmail.com>
2021-07-17 00:11:55 +0300

    Updated pruning to handle multiple convolutional layers (resnets).

--- imc_prune.py
+++ imc_prune.py
@@ -3,27 +3,37 @@
 import torch
 import warnings
 import torch_pruning as tp
+from torch.nn import modules
+from torchvision.models.resnet import BasicBlock
 
 warnings.filterwarnings("ignore")
 
 
-def prune_model(name='', model=None, dir_models='', suffix=''):
+def prune_model(name='', model=None, dir_models='', suffix='', im_size=224):
     print('\nPruning Model: ' + name + '...', end='\t')
 
-    # 1. setup strategy (L1 Norm)           <-- can also try tp.strategy.RandomStrategy()
     strategy = tp.strategy.L1Strategy()
+    DG = tp.DependencyGraph().build_dependency(model, example_inputs=torch.randn(1, 3, im_size, im_size))
 
-    # 2. build layer dependency for resnet18
-    DG = tp.DependencyGraph()
-    DG.build_dependency(model, example_inputs=torch.randn(1, 3, 224, 224))
+    def prune_conv(conv, amount=0.2):
+        pruning_index = strategy(conv.weight, amount=amount)
+        plan = DG.get_pruning_plan(conv, tp.prune_conv, pruning_index)
+        plan.exec()
 
-    # 3. get a pruning plan from the dependency graph.
-    pruning_idxs = strategy(model.conv1.weight, amount=0.4)     # or manually selected pruning_idxs=[2, 6, 9, ...]
-    pruning_plan = DG.get_pruning_plan(model.conv1, tp.prune_conv, idxs=pruning_idxs)
-    # print(pruning_plan)
+    block_prune_probs = [0.1, 0.1, 0.2, 0.2, 0.2, 0.2, 0.3, 0.3]
+    limit = len(block_prune_probs) - 1
+    blk_id = 0
+    for m in list(model.modules())[1:]:
+        if isinstance(m, BasicBlock):
+            prune_conv(m.conv1, block_prune_probs[blk_id])
+            prune_conv(m.conv2, block_prune_probs[blk_id])
+            if blk_id < limit - 1:
+                blk_id += 1
 
-    # 4. execute this plan (prune the model)
-    pruning_plan.exec()
+    # pruning_idxs = strategy(model.conv1.weight, amount=0.4)     # or manually selected pruning_idxs=[2, 6, 9, ...]
+    # pruning_plan = DG.get_pruning_plan(model.conv1, tp.prune_conv, idxs=pruning_idxs)
+
+    # pruning_plan.exec()
     print('COMPLETE')
 
     # 5. Save Model
